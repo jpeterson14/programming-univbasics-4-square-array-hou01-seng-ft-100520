45e8d1b3a2e739924589c61cd2b700e5fa43fc4d
Jake Peterson <jpeterson0514@gmail.com>
2020-08-13 18:44:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   counter = 0 
   new_array = []
-  while array[counter] < array.length do |integer|
-    new_array << integer**2
+  while array[counter] < array.length do
+    new_array << array * array 
     counter += 1
 end 
 end
